baa303949a21b8d7ea95ac060d817a340f6b5a95
Kulesko <Grygoriy.Kulesko@gmx.de>
2021-03-18 19:40:43 +0100

    initial commit: just placeholders

---
+++ Assets/Scripts/ui/HpIndicator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HpIndicator : MonoBehaviour
+{
+    private GameController _gameController;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
