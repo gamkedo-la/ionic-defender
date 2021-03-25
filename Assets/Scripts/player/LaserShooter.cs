cc05a2269d2671d21d83caac1db2bd6da01da07d
Kulesko <Grygoriy.Kulesko@gmx.de>
2021-03-25 20:30:49 +0100

    improvement(lase collision): using 2d math

--- Assets/Scripts/player/LaserShooter.cs
+++ Assets/Scripts/player/LaserShooter.cs
@@ -9,16 +9,15 @@ namespace player
     [SelectionBase]
     public class LaserShooter : MonoBehaviour
     {
-        private Transform LaserCenter;
-        public float LaserOffset = 1;
-
-        public Transform DebugHitPosition;
+        private Transform laserCenter;
+        public float laserOffset = 1;
+        public Transform debugHitPosition;
         // public Transform DebugHitPosition;
 
         // Start is called before the first frame update
         void Start()
         {
-            LaserCenter = transform;
+            laserCenter = transform;
         }
 
         // Update is called once per frame
@@ -28,28 +27,29 @@ namespace player
 
         public void ShootLaser(Vector3 endPoint)
         {
-            Vector3 extendedEndpoint = new Vector3(endPoint.x, endPoint.y, LaserCenter.transform.position.z);
-            Vector3 direction = (extendedEndpoint - LaserCenter.position).normalized;
-            direction.Scale(new Vector3(LaserOffset, LaserOffset, LaserOffset));
-            Vector3 laserOrigin = LaserCenter.position + direction;
+            Vector3 extendedEndpoint = new Vector3(endPoint.x, endPoint.y, laserCenter.transform.position.z);
+            Vector3 direction = (extendedEndpoint - laserCenter.position).normalized;
+            direction.Scale(new Vector3(laserOffset, laserOffset, laserOffset));
+            Vector3 laserOrigin = laserCenter.position + direction;
             // TODO limit angle
 
             Vector3 extendedDirection = new Vector3(direction.x, direction.y, direction.z);
             extendedDirection.Scale(new Vector3(20, 20, 20));
-            Vector3 laserExtended = LaserCenter.position + extendedDirection;
+            Vector3 laserExtended = laserCenter.position + extendedDirection;
 
             // Instantiate(Resources.Load("Debug/MarkerUndirectional"), laserOrigin, Quaternion.identity);
             ShootLaser(laserOrigin, laserExtended);
         }
 
-        private void ShootLaser(Vector3 laserOrigin, Vector3 laserExtended)
+        private void ShootLaser(Vector2 laserOrigin, Vector2 laserExtended)
         {
             Ray ray = new Ray(laserOrigin, laserExtended - laserOrigin);
-            RaycastHit hitInfo;
             Debug.DrawLine(laserOrigin, laserExtended, Color.magenta, Time.deltaTime);
 
             int LayerMask = UnityEngine.LayerMask.GetMask("Hitable");
-            if (Physics.Raycast(ray, out hitInfo, 1000f ,LayerMask))
+            RaycastHit2D hitInfo = Physics2D.Raycast(laserOrigin, laserExtended - laserOrigin, 1000f, LayerMask);
+            
+            if (hitInfo)
             {
                 var hitableEnemy = hitInfo.collider.gameObject.GetComponent<HitableEnemy>();
                 if (hitableEnemy != null)
@@ -57,7 +57,7 @@ namespace player
                     // hitableEnemy.takeDamage(100 * Time.deltaTime);
                 }
 
-                DebugHitPosition.position = hitInfo.point;
+                debugHitPosition.position = hitInfo.point;
             }
         }
     }
