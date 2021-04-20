58158b29adc6b2ff481a8f69f03df4c57c82ca03
fdottori <81397319+fdottori@users.noreply.github.com>
2021-04-19 23:12:28 -0300

    Added basic wave system.

--- Assets/EnemySpawn.cs
+++ Assets/EnemySpawn.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemySpawn : MonoBehaviour
 {
@@ -9,25 +10,50 @@ public class EnemySpawn : MonoBehaviour
     public float spawnMinX;
     public float spawnMaxX;
     public float spawnY;
+    public float enemiesPerWave;
+    public float reduceSpawnTime;
+    public Text waveCountText;
+    public float minSpawnTime;
 
     private float timer = 0f;
+    private int waveCount = 1;
+    private int enemiesSpawned = 0;
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        waveCountText.text = "Day " + waveCount;
     }
 
-    // Update is called once per frame
     void Update()
     {
         timer += Time.deltaTime;
 
         if (timer >= secondsToSpawn)
         {
-            Vector3 enemyPos = new Vector3(Random.Range(spawnMinX, spawnMaxX), spawnY, 0f);
             timer = 0F;
+            enemiesSpawned++;
+            
+            Vector3 enemyPos = new Vector3(Random.Range(spawnMinX, spawnMaxX), spawnY, 0f);
             Instantiate(enemy, enemyPos, Quaternion.identity);
 		}
-    }
+
+        if(enemiesSpawned >= enemiesPerWave)
+		{
+            if(secondsToSpawn - reduceSpawnTime > minSpawnTime)  // Avoid negative spawn time
+            {
+                secondsToSpawn -= reduceSpawnTime;
+
+                // Unity was doing weird things with the subtraction, 
+                // so this next bit will imit the value to 1 decimal place
+                secondsToSpawn = Mathf.Round(secondsToSpawn * 10) / 10; 
+            }
+            else
+			{
+                secondsToSpawn = minSpawnTime;
+            }
+            waveCount++;
+            enemiesSpawned = 0;
+            waveCountText.text = "Day " + waveCount;
+        } // Enf of if increase wave
+    } // End of Update
 }
