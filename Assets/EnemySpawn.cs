c5af1562f3c8f73b39a87d545ca98e15f7a74465
fdottori <81397319+fdottori@users.noreply.github.com>
2021-04-19 22:46:49 -0300

    Added basic enemy spawn.

---
+++ Assets/EnemySpawn.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    public GameObject enemy;
+    public float secondsToSpawn;
+    public float spawnMinX;
+    public float spawnMaxX;
+    public float spawnY;
+
+    private float timer = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= secondsToSpawn)
+        {
+            Vector3 enemyPos = new Vector3(Random.Range(spawnMinX, spawnMaxX), spawnY, 0f);
+            timer = 0F;
+            Instantiate(enemy, enemyPos, Quaternion.identity);
+		}
+    }
+}
