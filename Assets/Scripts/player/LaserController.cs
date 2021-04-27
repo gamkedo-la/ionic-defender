dd24747631b1798e68a414a16fca2c8460dc5120
fdottori <81397319+fdottori@users.noreply.github.com>
2021-04-26 21:52:51 -0300

    Destroy spawned enemies on overheat

--- Assets/Scripts/player/LaserController.cs
+++ Assets/Scripts/player/LaserController.cs
@@ -31,6 +31,11 @@ public class LaserController : MonoBehaviour
 
         if(overheat)
 		{
+            GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+			for (int i = 0; i < enemies.Length; i++)
+			{
+                Destroy(enemies[i]);
+			}
             currentHeat -= Time.deltaTime;
 		}
 
