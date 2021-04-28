836d55f5b7c8ec9b1aa1e288e4ef5bc78f3c91be
fdottori <81397319+fdottori@users.noreply.github.com>
2021-04-28 15:23:30 -0300

    Sky background

---
+++ Assets/Scripts/RotateSky.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSky : MonoBehaviour
+{
+    public float rotationSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+    }
+}
