0289b088a1d22f7115e04e2cdb72c873053d69c8
fdottori <81397319+fdottori@users.noreply.github.com>
2021-05-13 23:01:28 -0300

    Some more work on particle for explosions

---
+++ Assets/Scripts/SelfDestruct.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestruct : MonoBehaviour
+{
+    public float lifetime;
+    private float timer = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(timer >= lifetime)
+		{
+            Destroy(gameObject);
+		}
+    }
+}
