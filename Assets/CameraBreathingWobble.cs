b42eec52801e461ce886509b5e48f50838e4341d
Gamkedo <chris@gamkedo.com>
2021-06-24 18:11:21 -0700

    Camera "breathing" wobble added for title menu background

---
+++ Assets/CameraBreathingWobble.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBreathingWobble : MonoBehaviour
+{
+    Quaternion origRot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        origRot = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = origRot *
+            Quaternion.AngleAxis(Mathf.Sin(Time.timeSinceLevelLoad*0.3f) * 1.5f, Vector3.up)
+             *
+            Quaternion.AngleAxis(Mathf.Sin(Time.timeSinceLevelLoad * 0.17f) * 1.0f, Vector3.right);
+    }
+}
